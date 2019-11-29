fa4a872c8d57c8c75d0d957e50fd64963aacc198
Darcy NGUYEN <nguyendarcy@hotmail.fr>
2019-11-29 14:01:42 +0100

    TP Date

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,11 @@ package java8.ex07;
 import org.junit.Test;
 
 import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -19,14 +22,21 @@ public class DateAndTime_07_Test {
     public void test_date_to_localdate() throws Exception {
 
         // TODO Créer une date Java 1 (12/02/2017)
-        Date date = null;
+        Date date = new Date(2017-1900,1,12);
 
         // TODO transformer la date en Instant
+        
+        Instant ins = date.toInstant();
 
         // TODO transformer la date en LocalDate
+        
+        ZoneId currentZone = ZoneId.systemDefault();
 
-        LocalDate result = null;
-
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(ins,currentZone);
+        
+        LocalDate result = localDateTime.toLocalDate();
+        
+        
         // TODO valoriser les différentes variables afin de rendre le test passant
 
         assertThat(result.getYear(), is(2017));
