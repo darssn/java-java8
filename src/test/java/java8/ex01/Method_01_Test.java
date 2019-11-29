ce62d62043eaf95c5233022834d59b6534f49c75
Darcy NGUYEN <nguyendarcy@hotmail.fr>
2019-11-29 11:25:50 +0100

    tp interface

--- src/test/java/java8/ex01/Method_01_Test.java
+++ src/test/java/java8/ex01/Method_01_Test.java
@@ -15,8 +15,19 @@ public class Method_01_Test {
 
     // tag::IDao[]
     interface IDao {
+    	
         List<Person> findAll();
 
+       default int sumAge(){
+        	int sum = 0;
+        	
+        	for(Person p : findAll()){
+        		sum+= p.getAge();
+        	}
+    	   
+    	   return sum ;
+        	
+        }
         // TODO créer une méthode int sumAge()
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
     }
@@ -48,7 +59,7 @@ public class Method_01_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoA.sumAge();
 
         assert result == 210;
     }
@@ -59,7 +70,7 @@ public class Method_01_Test {
         DaoB daoB = new DaoB();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
 
         assert result == 5050;
 
--- src/test/java/java8/ex02/Method_02_Test.java
+++ src/test/java/java8/ex02/Method_02_Test.java
@@ -15,6 +15,16 @@ public class Method_02_Test {
     // tag::IDao[]
     interface IDao {
         List<Person> findAll();
+        
+        
+        default String format(){
+        	
+        	int nb = findAll().size();
+        	         	
+        	return "["+nb+" persons]";
+        	
+ 	
+        }
 
         // TODO créer une méthode String format()
         // TODO la méthode retourne une chaîne de la forme [<nb_personnes> persons]
@@ -31,6 +41,15 @@ public class Method_02_Test {
         public List<Person> findAll() {
             return people;
         }
+        
+        @Override
+        public String format(){
+        	
+        	
+        	
+        	return "DaoA"+IDao.super.format();
+        }
+        
 
         // TODO redéfinir la méthode String format()
         // TODO la méthode retourne une chaîne de la forme DaoA[<nb_personnes> persons]
@@ -44,9 +63,11 @@ public class Method_02_Test {
     public void test_daoA_format() throws Exception {
 
         DaoA daoA = new DaoA();
+       
 
         // TODO invoquer la méthode format() pour que le test soit passant
-        String result = null;
+        String result = daoA.format();
+        System.out.println(result);
 
         "DaoA[20 persons]".equals(result);
     }
